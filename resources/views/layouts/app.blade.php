fd5985cafb99637de55ec66e59ba70b260d5f691
Muhammad Rasyidi <muhammadrasyidi17@gmail.com>
2024-05-05 21:14:19 +0700

    add app icon on dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/sweetalert2.min.css') }}">
+    <link rel="icon" href="{{ asset('images/Handbag.png') }}">
 
     @vite('resources/css/app.css')
 
